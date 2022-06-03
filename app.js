3010669ed9c80d7bb07321e0dd0c0e9779926d04
SantiR <santiago.r.n@hotmail.com>
2022-06-03 11:34:49 -0300

    Dependencies install, folder diagram

--- app.js
+++ app.js
@@ -3,34 +3,64 @@ const axios = require('axios')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.get('/estudiantes', (req,res) => {
+//   const estudiantes = [
+//     {nombre: "Nicolás", apellido: "Ribeiro"},
+//     {nombre: "César", apellido: "Rolón"},
+//     {nombre: "Santiago", apellido: "Rodriguez"},
+//     {nombre: "Nati", apellido: "Ciraolo"},
+//   ]
+
+//     return res.send({ success: true, data: estudiantes })
+
+
+// })
+
+
+// app.get ('/pokemones', async (req,res) => {
+
+//   // const userName = req.params.name
+//   const response = await axios ("https://pokeapi.co/api/v2/pokemon/")
+
+//   const poke = response.data.results
+
+//   return res.send (poke[19].name);
+
+// })
+
+
+app.get("/pokemons", async (req, res) => {
+
+  const pokemons = await axios('https://pokeapi.co/api/v2/pokemon/');
+
+  const arrayPokemons =  pokemons.data.results;
+
+  const nombresPokemons  = []
+
+  for (let i = 0; i < arrayPokemons.length; i++) {
+
+      nombresPokemons.push(arrayPokemons[i].name);
+      
+
+  }
+
+  return res.send(nombresPokemons);
+
+});
+
+
+app.post("/usuarios", (req,res,next)=>{
+  try {
+    
+  } catch (error) {
+    
+  }
 })
 
-app.get('/estudiantes', (req,res) => {
-  const estudiantes = [
-    {nombre: "Nicolás", apellido: "Ribeiro"},
-    {nombre: "César", apellido: "Rolón"},
-    {nombre: "Santiago", apellido: "Rodriguez"},
-    {nombre: "Nati", apellido: "Ciraolo"},
-  ]
-
-    return res.send({ success: true, data: estudiantes })
-
-
-})
-
-
-app.get ('/repositorios/:userId', async (req,res) => {
-
-  const userId = req.params.userId
-  const response = await axios (`https://api.github.com/users/${userId}`)
-
-  const repos = await axios (response.data.repos_url)
-
-  return res.send (repos.data);
-
-})
 
 
 
